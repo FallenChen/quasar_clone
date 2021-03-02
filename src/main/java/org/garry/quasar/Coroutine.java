12360c37a8888383681667d9859302203b7be8b8
garrychen <chenymj23@gmail.com>
2021-03-02 11:33:24 +0800

    Coroutine init

--- src/main/java/org/garry/quasar/Coroutine.java
+++ src/main/java/org/garry/quasar/Coroutine.java
@@ -1,4 +1,37 @@
 package org.garry.quasar;
 
-public class Coroutine {
+import java.io.Serializable;
+
+/**
+ * A Coroutine is used to run a CoroutineProto
+ * It also provides a function to suspend a running Coroutine
+ *
+ * A Coroutine can be serialized if it's not running and all involved
+ * classes and data types are also {@link Serializable}
+ */
+public class Coroutine implements Runnable, Serializable {
+
+
+    public enum State
+    {
+        // The Coroutine has not yet been executed
+        NEW,
+        // The Coroutine is currently executing
+        RUNNING,
+        // The Coroutine has suspended it's execution
+        SUSPENDED,
+        // The Coroutine has finished it's run method
+        FINISHED
+    }
+
+
+    /**
+     * Suspend the currently running Coroutine on the calling thread
+     * @throws SuspendException
+     * @throws IllegalStateException
+     */
+    public static void yield() throws SuspendException, IllegalStateException
+    {
+        throw new Error("Calling function not instrumented");
+    }
 }
--- src/main/java/org/garry/quasar/CoroutineLocal.java
+++ src/main/java/org/garry/quasar/CoroutineLocal.java
@@ -1,5 +1,8 @@
 package org.garry.quasar;
 
+import java.lang.ref.WeakReference;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * This class provides coroutine-local variables. These variables differ from
  * their normal counterparts in that each coroutine that accesses one ( get or set method)
@@ -16,6 +19,68 @@ package org.garry.quasar;
  */
 public class CoroutineLocal<T> {
 
+    /**
+     * CoroutineLocals rely on per-coroutine linear-probe hash maps attached
+     * to each coroutine. The CoroutineLocal objects act as keys, searched via
+     * coroutineLocalHashCode. This is a custom hash code (useful only within CoroutineLocalMaps)
+     * that eliminates collisions in the common case where consecutively constructed ThreadLocals
+     * are used by the same threads, while remaining well-behaved in less common cases
+     */
+    private final int coroutineLocalHashCode = nextHashCode();
+
+    /**
+     * The next hash code to be given out.Updated atomically.Starts at zero
+     */
+    private static AtomicInteger nextHashCode =
+            new AtomicInteger();
+
+    /**
+     * The difference between successively generated hash codes
+     * turns implicit sequential thread-local IDs into near-optimally spread
+     * multiplicative hash values for power-of-two-sized tables
+     */
+    private static final int HASH_INCREMENT = 0x61c88647;
+
+    /**
+     * Returns the next hash code
+     * @return
+     */
+    private static int nextHashCode()
+    {
+        return nextHashCode.getAndAdd(HASH_INCREMENT);
+    }
+
+    /**
+     * Returns the current thread's "initial value" for this
+     * thread-local variable.This method will be invoked the first
+     * time a thread accesses the variable with the {@link #get}
+     * method, unless the thread previously invoked the {@link #set}
+     * method, in which case the <tt>initialValue</tt> method will not
+     * be invoked for the thread. Normally, this method is invoked at
+     * most once per thread, but it may be invoked again in case of
+     * subsequent invocations of {@link #remove} followed by {@link #get}
+     *
+     * This implementation simply returns null; if the
+     * programmer desires thread-local variables to have an initial
+     * value other than null, ThreadLocal must be subclassed, and this method overridden
+     * Typically, an anonymous inner calls will be used.
+     *
+     */
+    protected T initialValue()
+    {
+        return null;
+    }
+
+    public CoroutineLocal()
+    {
+
+    }
+
+    public T get()
+    {
+
+    }
+
 
     /**
      * ThreadLocalMap is a customized hash map suitable only for
@@ -27,6 +92,33 @@ public class CoroutineLocal<T> {
      */
     static class CoroutineLocalMap
     {
+        /**
+         * The entries in this hash map extend WeakReference, using
+         * its main ref field as the key (which is always a ThreadLocal object).
+         * Note that null keys(i.e. entry.get() == null) mean that the key is no longer referenced,
+         * so the entry can be expunged from table. Such entries are referred to as "stale entries"
+         * int the code that follows
+         */
+        static class Entry extends WeakReference<CoroutineLocal>
+        {
+            Object value;
+            // The value associated with this ThreadLocal
+            Entry(CoroutineLocal k, Object v)
+            {
+                super(k);
+                value = v;
+            }
+        }
 
+        /**
+         * The initial capacity -- MUST be a power of two
+         */
+        private static final int INITIAL_CAPACITY = 16;
+
+        /**
+         * The table, resized as necessary
+         * table.length MUST always be a power of two
+         */
+        private Entry[] table;
     }
 }
